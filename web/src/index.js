410ec89c0a5869f25f0a7b5fbab2410208df2e4f
pallomakcgualter <pallomakcgualter@gmail.com>
2023-02-09 18:10:12 -0300

    feat: defined global style

--- web/src/index.js
+++ web/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyle from './styles/global';
+import { Container, Content } from './styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <GlobalStyle />
-      <App />
+      <Container>
+        <Content>
+          <GlobalStyle />
+          <App />
+        </Content>
+      </Container>
   </React.StrictMode>
 );
